ee7dedd6bfc6c2ba2e95d3d0c092408eeb83252f
Luis Miguel <9luismiguel@gmail.com>
2023-06-23 10:16:32 -0700

    finished all changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import head_shot from "../assets/head_shot.JPG";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+    <div className="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src={head_shot}
+          alt="No Pic"
+          width="200" height="200"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Luis Migurel</div>
+        <div className="brief_description">
+           I love staying active, going to the gym, swimming, playing soccer.
+           I'm always trying to improve myself, culturally, physically, and spiritually.
+        </div>
+      </div>
+    </div>
+  </div>
       </div>
     )
   }
